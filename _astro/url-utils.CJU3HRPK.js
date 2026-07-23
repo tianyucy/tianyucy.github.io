2f22f69c688f72fcb1c71919956fc199b6c06649
tianyucy <tianyucy@users.noreply.github.com>
2026-07-23 09:16:09 +0000

    Auto Deploy: feat: use local favorite anime data tianyucy/web-main@18d7584ae6154c2e3067467d53441641193c410c

---
+++ _astro/url-utils.CJU3HRPK.js
@@ -0,0 +1,1 @@
+import"./translation.DzYouIHq.js";import"./config.xfq0CaQQ.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function u(r){return t("","/",r)}export{u as n,a as t};
--- _astro/url-utils.CK8Rv1RB.js
+++
@@ -1,1 +0,0 @@
-import"./translation.CEmNxKsS.js";import"./config.CbHSTgQq.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function u(r){return t("","/",r)}export{u as n,a as t};
