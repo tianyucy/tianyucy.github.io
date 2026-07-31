3ae8f14e1714bc2b063799e6519c230391b19ac7
tianyucy <tianyucy@users.noreply.github.com>
2026-07-31 04:14:40 +0000

    Auto Deploy: feat: replace Pio with official Live2D widget tianyucy/web-main@1a6c2e8e650eaeb1fe6a1fcd06543bb6c3166c6e

---
+++ _astro/url-utils.BE43I8cJ.js
@@ -0,0 +1,1 @@
+import"./translation.D9HRPs37.js";import"./config.apv-6MIk.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function u(r){return t("","/",r)}export{u as n,a as t};
